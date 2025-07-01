960f668117f3d7806941b9f370cc7be65b37ff4d
Akshay Gohrava <akshaygohrava@gmail.com>
2025-07-01 22:44:55 +0530

    An Calculator Update

--- Java Calculator/Calculator.java
+++ Java Calculator/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
     public static void main(String args[]) {
-        System.out.println("Hello, This is Calculator Here");
+        System.out.println("Hello, This is Calculator Based On Java ,By Akshay-Gohrava");
     }
  }
