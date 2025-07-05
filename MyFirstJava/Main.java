5d224a4a7a5a8e805fa6618a94709fbbfd40c0d6
Akshay Gohrava <akshaygohrava@example.com>
2025-07-05 19:43:52 +0530

    MyFirstJava

--- MyFirstJava/Main.java
+++ MyFirstJava/Main.java
@@ -9,9 +9,9 @@ public class Main {
         char firstletter = 'A';
 
         System.out.println("My Name is " + name);
-        System.err.println("I m " + age + " Year Old");
-        System.err.println("My Body Weight is " + weight);
-        System.err.println("My Love For MyMaa : " + love);
-        System.err.println("My First Letter is :" + firstletter);
+        System.out.println("I m " + age + " Year Old");
+        System.out.println("My Body Weight is " + weight);
+        System.out.println("My Love For MyMaa : " + love);
+        System.out.println("My First Letter is :" + firstletter);
     }
 }
