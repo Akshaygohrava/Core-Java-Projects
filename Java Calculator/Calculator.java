f2238072a0aefbb8f796451b131765b307925389
Akshay Gohrava <akshaygohrava@example.com>
2025-07-05 19:36:19 +0530

    MyFirstJava

--- Java Calculator/Calculator.java
+++ Java Calculator/Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello, This is Calculator Based On Java ,By Akshay-Gohrava");
     }
  }
+
+
+
+
+
+ 
---
+++ MyFirstJava/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello, This is Akshay Here Started Java");
+
+        String name = "AKshay Gohrava";
+        int age = 24;
+        float weight = 85.77f;
+        boolean love = true;
+        char firstletter = 'A';
+
+        System.out.println("My Name is " + name);
+        System.err.println("I m " + age + " Year Old");
+        System.err.println("My Body Weight is " + weight);
+        System.err.println("My Love For MyMaa : " + love);
+        System.err.println("My First Letter is :" + firstletter);
+    }
+}
